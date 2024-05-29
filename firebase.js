18fc92817ef077338e239ca215287c5de7d4877f
Ati <ativassileva@gmail.com>
2024-05-29 16:20:00 +0300

    add files

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAhMfZTr0tPwXpKQS1nTolmeoI5_WjPv4",
+  authDomain: "saltana-web.firebaseapp.com",
+  projectId: "saltana-web",
+  storageBucket: "saltana-web.appspot.com",
+  messagingSenderId: "463712394415",
+  appId: "1:463712394415:web:0fcb904f1681fde8e1085a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
